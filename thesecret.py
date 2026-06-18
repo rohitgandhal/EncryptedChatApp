25cdea0320675aa9cf7d9f2e4a558047ec39741b
rohitgandhal <133295900+rohitgandhal@users.noreply.github.com>
2026-06-18 12:55:33 +0530

    Delete thesecret.py

--- thesecret.py
+++
@@ -1,9 +0,0 @@
-def interest(n, months, z=500):
-    z = z/94
-    print(z, "-> Starting point") 
-    for i in range(n):
-        z*=2
-        print(z,"->",months[i])
-
-months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-interest(12,months)
